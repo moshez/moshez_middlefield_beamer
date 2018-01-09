f884d156c7209d9f573b395275b8fde77653d652
Moshe Zadka <zadka.moshe@gmail.com>
2018-01-08 18:16:25 -0800

    +stuff

--- src/moshez_middlefield_beamer/_impl.py
+++ src/moshez_middlefield_beamer/_impl.py
@@ -14,17 +14,6 @@ import middlefield
     parser=command('',
                    what=option(type=typing.List[str],
                                have_default=True)),
-    dependencies=['echo_printer'])
-def echo(args, dependencies):
-    """
-    Print out arguments
-    """
-    dependencies['echo_printer'](*args['what'])
-
-
-@middlefield.COMMANDS.dependency()
-def echo_printer(_dependencies, _maybe_dependencies):
-    """
-    Define what printing means
-    """
-    return print
+    )
+def beamer(args, dependencies):
+    return
--- src/moshez_middlefield_beamer/_version.py
+++ src/moshez_middlefield_beamer/_version.py
@@ -1,11 +1,11 @@
 """
-Provides middlefield_echo version information.
+Provides moshez_middlefield_beamer version information.
 """
 
 # This file is auto-generated! Do not edit!
-# Use `python -m incremental.update middlefield_` to change this file.
+# Use `python -m incremental.update moshez_middlefield_beamer` to change this file.
 
 from incremental import Version
 
-__version__ = Version('moshez_middlefield_beamer', 18, 1, 0)
+__version__ = Version('moshez_middlefield_beamer', 18, 1, 0, release_candidate=1)
 __all__ = ["__version__"]
