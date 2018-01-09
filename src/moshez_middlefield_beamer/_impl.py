2c771fae4461a384bb7300f7ae7c37cd5071d973
Moshe Zadka <zadka.moshe@gmail.com>
2018-01-08 18:39:30 -0800

    froop

--- src/moshez_middlefield_beamer/_impl.py
+++ src/moshez_middlefield_beamer/_impl.py
@@ -4,7 +4,10 @@ Demonstration of middlefield plugins
 
 from __future__ import print_function
 
+import os
+import shutil
 import typing
+import subprocess
 
 from caparg import command, option
 import middlefield
@@ -22,5 +25,15 @@ def beamer(args, dependencies):
     input = args.get('input', 'talk.tex')
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
-    shutil.copy(input, os.path.join(outdir, 'talk.tex'))
-    executor.command(['pdflatex', 'talk.tex']).batch(cwd=outdir)
+    with open(input, 'rb') as filep:
+       source = filep.read()
+    with open(os.path.join(outdir, 'talk.tex'), 'wb') as filep:
+       filep.write(b'\\documentclass{beamer}\n')
+       filep.write(source)
+    xctor.command(['pdflatex', 'talk.tex']).batch(cwd=outdir)
+    with open(os.path.join(outdir, 'handout.tex'), 'wb') as filep:
+       filep.write(b'\\documentclass{article}\n')
+       filep.write(b'\\usepackage{beamerarticle}\n')
+       filep.write(source)
+    xctor.command(['pdflatex', 'talk.tex']).batch(cwd=outdir)
+    xctor.command(['pdflatex', 'handout.tex']).batch(cwd=outdir)
