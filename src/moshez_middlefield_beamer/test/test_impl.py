78a9aa611234b7a991627b562a3b48c88ed858c3
Moshe Zadka <zadka.moshe@gmail.com>
2018-02-03 21:05:22 -0800

    test

--- src/moshez_middlefield_beamer/test/test_impl.py
+++ src/moshez_middlefield_beamer/test/test_impl.py
@@ -20,6 +20,15 @@ class FakeShell(object):
             cwd = kwargs['cwd']
             src = os.path.join(cwd, _cmd[1])
             dest = os.path.splitext(src)[0] + '.pdf'
+            with open(src, 'rb') as inp:
+                tex = inp.read()
+            lines = tex.decode('ascii').splitlines()
+            for line in lines:
+                if not line.strip().startswith('\lstinputlisting'):
+                    continue
+                file_name = os.path.join(cwd, line.split('{')[1].split('}')[0])
+                if not os.path.exists(file_name):
+                    raise ValueError("Cannot find", file_name)
             shutil.copy(src, dest)
             return
         raise NotImplementedError(_cmd, args, kwargs)
@@ -29,18 +38,26 @@ class ImplTest(unittest.TestCase):
 
     def test_beamer(self):
         executor = seashore.Executor(FakeShell())
-        self.addCleanup(lambda: shutil.rmtree(self.build_dir))
-        self.build_dir = tempfile.mkdtemp()
-        self.output_dir = os.path.join(self.build_dir, 'build')
-        with tempfile.NamedTemporaryFile() as filep:
-            filep.write(b'this is like a source\n')
+        self.addCleanup(lambda: shutil.rmtree(build_dir))
+        build_dir = tempfile.mkdtemp()
+        output_dir = os.path.join(build_dir, 'build')
+        self.addCleanup(lambda: shutil.rmtree(source_dir))
+        source_dir = tempfile.mkdtemp()
+        source_name = os.path.join(self.source_dir, 'talk.tex')
+        code_name = os.path.join(self.source_dir, 'foo.py')
+        with open(source_name, 'wb) as filep:
+            filep.write(b'''this is like a source
+                            \lstinputlisting{foo.py}
+                         ''')
             filep.flush()
-            _impl.beamer(dict(input=filep.name,
-                              outdir=self.output_dir),
-                         dict(executor=executor))
-        with open(os.path.join(self.output_dir, 'talk.pdf')) as filep:
+        with open(code_name, 'wb) as filep:
+            filep.write(b'import this\n')
+        _impl.beamer(dict(input=source_name
+                          outdir=output_dir),
+                     dict(executor=executor))
+        with open(os.path.join(output_dir, 'talk.pdf')) as filep:
             slides = filep.read().splitlines()
-        with open(os.path.join(self.output_dir, 'handout.pdf')) as filep:
+        with open(os.path.join(output_dir, 'handout.pdf')) as filep:
             handout = filep.read().splitlines()
         self.assertEquals(slides.pop(), 'this is like a source')
         self.assertEquals(handout.pop(), 'this is like a source')
