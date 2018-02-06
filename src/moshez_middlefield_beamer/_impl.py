767ed4cf706544cc333ce17c8a9311ca242cef07
Moshe Zadka <zadka.moshe@gmail.com>
2018-02-05 19:59:33 -0800

    +stuff

--- src/moshez_middlefield_beamer/_impl.py
+++ src/moshez_middlefield_beamer/_impl.py
@@ -5,6 +5,7 @@ Demonstration of middlefield plugins
 from __future__ import print_function
 
 import os
+import shutil
 
 from caparg import command, option
 import middlefield
@@ -22,10 +23,18 @@ def beamer(args, dependencies):
     xctor = dependencies['executor']
     outdir = os.path.abspath(args.get('outdir', 'build'))
     my_input = args.get('input', 'talk.tex')
+    my_input_dir = os.path.dirname(os.path.abspath(my_input))
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     with open(my_input, 'rb') as filep:
         source = filep.read()
+    for line in source.decode('utf-8').splitlines():
+        line = line.strip()
+        if not line.startswith(r'\lstinputlisting'):
+            continue
+        fname = line.split('{')[1].split('}')[0]
+        shutil.copy(os.path.join(my_input_dir, fname),
+                    os.path.join(outdir, fname))
     with open(os.path.join(outdir, 'talk.tex'), 'wb') as filep:
         filep.write(b'\\documentclass[ignorenonframetext]{beamer}\n')
         filep.write(source)
--- src/moshez_middlefield_beamer/test/test_impl.py
+++ src/moshez_middlefield_beamer/test/test_impl.py
@@ -24,7 +24,7 @@ class FakeShell(object):
                 tex = inp.read()
             lines = tex.decode('ascii').splitlines()
             for line in lines:
-                if not line.strip().startswith('\lstinputlisting'):
+                if not line.strip().startswith(r'\lstinputlisting'):
                     continue
                 file_name = os.path.join(cwd, line.split('{')[1].split('}')[0])
                 if not os.path.exists(file_name):
@@ -46,9 +46,8 @@ class ImplTest(unittest.TestCase):
         source_name = os.path.join(source_dir, 'talk.tex')
         code_name = os.path.join(source_dir, 'foo.py')
         with open(source_name, 'wb') as filep:
-            filep.write(b'''this is like a source
-                            \lstinputlisting{foo.py}
-                         ''')
+            filep.write(br'''\lstinputlisting{foo.py}
+                             this is like a source''')
             filep.flush()
         with open(code_name, 'wb') as filep:
             filep.write(b'import this\n')
@@ -59,7 +58,7 @@ class ImplTest(unittest.TestCase):
             slides = filep.read().splitlines()
         with open(os.path.join(output_dir, 'handout.pdf')) as filep:
             handout = filep.read().splitlines()
-        self.assertEquals(slides.pop(), 'this is like a source')
-        self.assertEquals(handout.pop(), 'this is like a source')
+        self.assertEquals(slides.pop().strip(), 'this is like a source')
+        self.assertEquals(handout.pop().strip(), 'this is like a source')
         self.assertEquals(slides.pop(0).split('{')[-1][:-1], 'beamer')
         self.assertEquals(handout.pop(0).split('{')[-1][:-1], 'article')
